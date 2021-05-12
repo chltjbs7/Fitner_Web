a2b7599e398729149057af0807c34ddbfbf60600
jes966 <44824406+jes966@users.noreply.github.com>
2021-05-13 00:34:54 +0900

    youtube_api(video page)

--- workspace/eunseo/fitnerproject/fitnerproject/urls.py
+++ workspace/eunseo/fitnerproject/fitnerproject/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from django.conf.urls import include
+from fitnerapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('fitnerapp.urls'))
+    path('', include('fitnerapp.urls')), #폴더.파일명
+    #즉, user/ 이하 url들은 myuser폴더의 urls에서 관리하도록한다.라는 설정
+    path('', home),
 ]
